920b3e78b773b5f6cc140d2f53919ede2c502322
Anar-Mammadzada <anarmammadza@gmail.com>
2024-11-10 21:06:06 +0400

    initial commit for frontend side

---
+++ angular-ecommerce/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { ProductService } from './services/product.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [
+    provideHttpClient(withInterceptorsFromDi()),
+    ProductService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
