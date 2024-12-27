5556c276d9d941f63c6f9ed009fe6baa754331ef
Anar-Mammadzada <anarmammadza@gmail.com>
2024-12-27 11:55:01 +0400

    Enhance html file and product-list-componenet for pagination

--- angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -18,7 +18,7 @@ export class ProductListComponent implements OnInit{
   searchMode : boolean = false;
 
   thePageNumber : number = 1;
-  thePageSize : number = 10;
+  thePageSize : number = 5;
   theTotalElements : number = 0;
 
   constructor(private productService : ProductService,
@@ -82,6 +82,12 @@ export class ProductListComponent implements OnInit{
       this.theTotalElements = data.page.totalElements;
     }
    );
+  }
+
+  updatePageSize(pageSize : string){
+    this.thePageSize = +pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
   }
 
 }
